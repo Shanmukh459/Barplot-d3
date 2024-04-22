1f5f724ad00fe4501c6168e3e78d4268b1257a0e
Shanmukh459 <saikrrish459@gmail.com>
2024-04-22 16:43:54 -0700

    getting data from the gist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import { useState } from 'react'
+import { csv } from 'd3'
+import { useState, useEffect } from 'react'
+
+const csvUrl = "https://gist.githubusercontent.com/Shanmukh459/2aee3b1c120bb3cf829b756e98e46804/raw/fd2320218c8bdd43220285524528161ce1e938e1/UN_Population_2019.csv"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    csv(csvUrl).then(data => setData(data))
+
+  }, [])
+
+  console.log(data)
 
   return (
     <h1>Hello</h1>
