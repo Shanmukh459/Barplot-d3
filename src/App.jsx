abf38488139d900f66a624fda19e7d6eab66ff7a
Shanmukh459 <saikrrish459@gmail.com>
2024-04-23 10:57:45 -0700

    ticks on x

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { csv } from 'd3'
 import { useState, useEffect } from 'react'
-import { scaleBand, scaleLinear, max } from 'd3'
+import { scaleBand, scaleLinear, max, line } from 'd3'
 
 const csvUrl = "https://gist.githubusercontent.com/Shanmukh459/2aee3b1c120bb3cf829b756e98e46804/raw/fe3210df00d80c0fbbaa7b39f6b32a62ce1c9a1e/UN_Population_2100.csv"
 
 const width = window.screen.width
-const height = window.screen.height
+const height = 1020
+
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 20
+}
 
 function App() {
 
@@ -24,19 +31,41 @@ function App() {
   if(!data) {
     return <pre>Loading..!</pre>
   }
-  console.log(data[0])
+
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.left - margin.right
 
   const yScale = scaleBand()
     .domain(data.map(d => d.Country))
-    .range([0, height])
+    .range([0, innerHeight])
 
   const xScale = scaleLinear()
     .domain([0, max(data, d => d.Population)])
-    .range([0, width])
+    .range([0, innerWidth])
 
   return (
     <svg width={width} height={height}>
-      {data.map(d => <rect key={d.Country} x={0} y={yScale(d.Country)} width={xScale(d.Population)} height={yScale.bandwidth()} />)}
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        {xScale.ticks().map(tickValue => (
+          <line 
+            x1={xScale(tickValue)} 
+            y1={0} 
+            x2={xScale(tickValue)} 
+            y2={innerHeight} 
+            stroke="black"
+          />
+        ))}
+        {data.map(d => (
+          <rect 
+            key={d.Country} 
+            x={0} 
+            y={yScale(d.Country)} 
+            width={xScale(d.Population)} 
+            height={yScale.bandwidth()} 
+          />
+        ))}
+
+      </g>
     </svg>
   )
 }
