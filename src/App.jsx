497fc410fb4b75fcbfffebfb45ba2dd4c63d7bad
Shanmukh459 <saikrrish459@gmail.com>
2024-04-23 11:41:40 -0700

    refactoriing complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { scaleBand, scaleLinear, max, line } from "d3";
 import { useData } from "./useData";
 import { AxisBottom } from "./AxisBottom";
 import { AxisLeft } from "./AxisLeft";
+import { Marks } from "./Marks";
 
 const width = window.screen.width;
 const height = 1020;
@@ -24,12 +25,13 @@ function App() {
   const innerHeight = height - margin.top - margin.bottom;
   const innerWidth = width - margin.left - margin.right;
 
-  const yScale = scaleBand()
-    .domain(data.map((d) => d.Country))
-    .range([0, innerHeight]);
+  const xValue = (d) => d.Population;
+  const yValue = (d) => d.Country;
+
+  const yScale = scaleBand().domain(data.map(yValue)).range([0, innerHeight]);
 
   const xScale = scaleLinear()
-    .domain([0, max(data, (d) => d.Population)])
+    .domain([0, max(data, xValue)])
     .range([0, innerWidth]);
 
   return (
@@ -37,15 +39,13 @@ function App() {
       <g transform={`translate(${margin.left}, ${margin.top})`}>
         <AxisBottom xScale={xScale} innerHeight={innerHeight} />
         <AxisLeft yScale={yScale} />
-        {data.map((d) => (
-          <rect
-            key={d.Country}
-            x={0}
-            y={yScale(d.Country)}
-            width={xScale(d.Population)}
-            height={yScale.bandwidth()}
-          />
-        ))}
+        <Marks
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          xValue={xValue}
+          yValue={yValue}
+        />
       </g>
     </svg>
   );
